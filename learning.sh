5d82146a47a17b7856368796c5dfbba8ef55a440
likith586 <98764402+likith586@users.noreply.github.com>
2022-09-20 01:22:34 +0530

    Update learning.sh

--- learning.sh
+++ learning.sh
@@ -2,6 +2,6 @@ I am learning devops
 I wish to take more salary
 I want to become a devops engineer.
 i want to earn more money
-I want to be well setteled in my life
+I want to be my life
 i want to build a big house
 I want to play cricket
